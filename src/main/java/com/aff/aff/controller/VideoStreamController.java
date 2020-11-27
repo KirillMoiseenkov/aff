32b3efa095c60d2db0adb427a87be850f5573f40
kirillmoiseenkov <kirillmoiseenkov@mail.ru>
2020-11-27 13:36:09 +0400

    change mapping

--- src/main/java/com/aff/aff/controller/VideoStreamController.java
+++ src/main/java/com/aff/aff/controller/VideoStreamController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/video")
+@RequestMapping("/audio")
 @RequiredArgsConstructor
 public class VideoStreamController {
 
